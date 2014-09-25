670363ce9fc1dc74b19385028367d9a73efa5778
protonpopsicle <smeisburger@gmail.com>
2014-09-25 14:16:35 -0400

    updated post-commit hook

--- post-commit.sh
+++ post-commit.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "Type path to scottmeisburger.com repo, followed by [ENTER]:"
+echo "Type full path to scottmeisburger.com repo:"
 
 # Allows us to read user input below, assigns stdin to keyboard
 exec < /dev/tty
@@ -9,5 +9,10 @@ read path
 
 cp *.pdf $path/
 
-cd $path && git stash && git add --all && git commit -m 'synced resume doc'
-cd $path && git pull && git push && git stash apply
+cd $path
+git stash
+git add --all
+git commit -m 'synced resume doc'
+git pull
+git push
+git stash apply
