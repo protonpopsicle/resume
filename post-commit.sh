c240b74d8f06b42a9cfd95c2688823ff2150d8f3
protonpopsicle <smeisburger@gmail.com>
2014-12-14 14:43:12 -0500

    updated post commit hook

--- post-commit.sh
+++ post-commit.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 
-echo "What's the full path to your site's repo Scott?:"
-
-# Allows us to read user input below, assigns stdin to keyboard
-exec < /dev/tty
-
-read path
+$path = ../protonpopsicle.github.io
 
 cp -v *.pdf $path/
 
