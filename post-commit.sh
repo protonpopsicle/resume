b37045be1883b513bec2bd7621915e31d37bda4a
protonpopsicle <smeisburger@gmail.com>
2014-12-14 15:09:47 -0500

    added Samsung to resume

--- post-commit.sh
+++ post-commit.sh
@@ -2,9 +2,11 @@
 
 path=../protonpopsicle.github.io/
 
+cd $path && git stash
 cp -v *.pdf $path
-
 cd $path && git add --all && git commit -m 'synced resume doc'
-cd $path && git pull && git push
+cd $path && git pull
+cd $path && git push
+cd $path && git stash apply
 
 echo '...Yay!'
